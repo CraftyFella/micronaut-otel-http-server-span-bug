a4952252f465b5657a8beb4f4d854f0cd31a9a10
Dave Craft <david.craft@marks-and-spencer.com>
2024-01-24 16:51:19 +0000

    Make it the same as the other branch. (which works)

--- src/main/kotlin/com/example/ApiController.kt
+++ src/main/kotlin/com/example/ApiController.kt
@@ -5,46 +5,32 @@ import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.PathVariable
+import io.opentelemetry.api.trace.Span
 import io.opentelemetry.api.trace.Tracer
 import reactor.core.publisher.Mono
 import io.opentelemetry.instrumentation.annotations.WithSpan
 
 @Controller("/api")
-open class ApiController(private val downstreamService: DownstreamService, private val dbRepository: DbRepository, private val tracer: Tracer) {
+open class ApiController(private val downstreamService: DownstreamService, private val dbRepository: DbRepository) {
     @Get(uri = "books/{bookId}")
     fun getBookById(@PathVariable(value = "bookId") bookId: String): Mono<HttpResponse<Book>>
     {
-        val span = tracer.spanBuilder("dave").startSpan()
-        val current = span.makeCurrent()
         return downstreamService.happy()
             .flatMap {
                 findBookById(bookId)
             }.map<HttpResponse<Book>?> {
                 HttpResponse.ok()
             }
-            .doOnNext {
-                current.close()
-                span.end()
-            }
     }
 
     @WithSpan
-    open fun findBookById(id: String): Mono<Book> =
-        dbRepository.findAllBooks().map { books ->
+    open fun findBookById(id: String): Mono<Book> {
+        Span.current().updateName("findBookById $id")
+        return dbRepository.findAllBooks().map { books ->
             books.single {
                 it.id == id
             }
         }
+    }
 
-//    @Get(uri = "authors/{authorId}")
-//    fun getAuthorById(@PathVariable(value = "authorId") authorId: String): Mono<HttpResponse<Author>>
-//    {
-//        return db.findAllAuthors()
-//            .map {
-//                it.single { author -> author.id == authorId }
-//            }
-//            .map {
-//                HttpResponse.ok(it)
-//            }
-//    }
 }
