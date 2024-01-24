70ecac15416da4e25c2558aa2036323d46cb90a5
Dave Craft <david.craft@marks-and-spencer.com>
2024-01-24 16:49:04 +0000

    How to update a span name after it's created

--- src/main/kotlin/com/example/ApiController.kt
+++ src/main/kotlin/com/example/ApiController.kt
@@ -6,6 +6,7 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.PathVariable
 import io.micronaut.tracing.annotation.NewSpan
+import io.opentelemetry.api.trace.Span
 import reactor.core.publisher.Mono
 
 @Controller("/api")
@@ -16,28 +17,19 @@ open class ApiController(private val downstreamService: DownstreamService, priva
         return downstreamService.happy()
             .flatMap {
                 findBookById(bookId)
-            }.map {
+            }.map<HttpResponse<Book>?> {
                 HttpResponse.ok()
             }
     }
 
     @NewSpan("my-fake-db")
-    open fun findBookById(id: String) =
-        dbRepository.findAllBooks().map { books ->
+    open fun findBookById(id: String): Mono<Book> {
+        Span.current().updateName("finding book $id")
+        return dbRepository.findAllBooks().map { books ->
             books.single {
                 it.id == id
             }
         }
+    }
 
-//    @Get(uri = "authors/{authorId}")
-//    fun getAuthorById(@PathVariable(value = "authorId") authorId: String): Mono<HttpResponse<Author>>
-//    {
-//        return db.findAllAuthors()
-//            .map {
-//                it.single { author -> author.id == authorId }
-//            }
-//            .map {
-//                HttpResponse.ok(it)
-//            }
-//    }
 }
