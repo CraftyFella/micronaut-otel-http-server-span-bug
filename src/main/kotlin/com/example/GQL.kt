e2b8e3109b294c185dd3623e498d7f8919847118
Michiel Boekhoff <michielalexanderb@gmail.com>
2024-01-26 07:06:05 +0000

    Fix GraphQL scope issue

--- src/main/kotlin/com/example/GQL.kt
+++ src/main/kotlin/com/example/GQL.kt
@@ -1,19 +1,29 @@
 package com.example
 
+import graphql.ExecutionInput
+import graphql.ExecutionResult
 import graphql.GraphQL
 import graphql.schema.DataFetcher
 import graphql.schema.DataFetchingEnvironment
 import graphql.schema.GraphQLSchema
-import graphql.schema.idl.RuntimeWiring
-import graphql.schema.idl.SchemaGenerator
-import graphql.schema.idl.SchemaParser
-import graphql.schema.idl.TypeDefinitionRegistry
-import graphql.schema.idl.TypeRuntimeWiring
+import graphql.schema.idl.*
+import io.micronaut.configuration.graphql.GraphQLExecutionInputCustomizer
+import io.micronaut.configuration.graphql.GraphQLInvocation
+import io.micronaut.configuration.graphql.GraphQLInvocationData
+import io.micronaut.context.BeanProvider
 import io.micronaut.context.annotation.Bean
 import io.micronaut.context.annotation.Factory
+import io.micronaut.context.annotation.Primary
+import io.micronaut.core.async.publisher.Publishers
 import io.micronaut.core.io.ResourceResolver
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.MutableHttpResponse
 import jakarta.inject.Singleton
+import org.dataloader.DataLoaderRegistry
+import org.reactivestreams.Publisher
 import org.slf4j.LoggerFactory
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.concurrent.CompletableFuture
@@ -50,7 +60,8 @@ class GraphQLFactory {
             val runtimeWiring = RuntimeWiring.newRuntimeWiring()
                 .type(
                     TypeRuntimeWiring.newTypeWiring("Query")
-                    .dataFetcher("bookById", graphQLDataFetchers.bookByIdDataFetcher()))
+                        .dataFetcher("bookById", graphQLDataFetchers.bookByIdDataFetcher())
+                )
                 .build()
             val schemaGenerator = SchemaGenerator()
             val graphQLSchema = schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring)
@@ -63,5 +74,51 @@ class GraphQLFactory {
 
     companion object {
         private val LOG = LoggerFactory.getLogger(GraphQLFactory::class.java)
+    }
+}
+
+@Singleton
+class FixedGraphQLCustomizer : GraphQLExecutionInputCustomizer {
+    override fun customize(
+        executionInput: ExecutionInput,
+        httpRequest: HttpRequest<*>?,
+        httpResponse: MutableHttpResponse<String>?
+    ): Publisher<ExecutionInput> =
+        Flux.just(executionInput)
+}
+
+@Singleton
+@Primary
+class FixedGraphQLInvocation(
+    private val graphQL: GraphQL,
+    private val customizer: GraphQLExecutionInputCustomizer,
+    private val dataLoaderRegistry: BeanProvider<DataLoaderRegistry>?
+) : GraphQLInvocation {
+    override fun invoke(
+        invocationData: GraphQLInvocationData?,
+        httpRequest: HttpRequest<*>?,
+        httpResponse: MutableHttpResponse<String>?
+    ): Publisher<ExecutionResult> {
+        val executionInputBuilder = ExecutionInput.newExecutionInput()
+            .query(invocationData!!.query)
+            .operationName(invocationData.operationName)
+            .variables(invocationData.variables)
+        if (dataLoaderRegistry != null) {
+            executionInputBuilder.dataLoaderRegistry(dataLoaderRegistry.get())
+        }
+        val executionInput = executionInputBuilder.build()
+        return Flux
+            .from(customizer.customize(executionInput, httpRequest, httpResponse))
+            .flatMap { customizedExecutionInput: ExecutionInput? ->
+                Mono.fromFuture {
+                    try {
+                        return@fromFuture graphQL.executeAsync(customizedExecutionInput)
+                    } catch (e: Throwable) {
+                        val future: CompletableFuture<ExecutionResult> = CompletableFuture()
+                        future.completeExceptionally(e)
+                        return@fromFuture future
+                    }
+                }
+            }
     }
 }
